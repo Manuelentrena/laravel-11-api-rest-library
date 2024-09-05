25a5b8f942472ee55eb11cf4cd0f3d2667658581
EntrenaDev <manuel.entrena@gmail.com>
2024-09-05 10:17:17 +0200

    feat: :necktie: Added model logic

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,37 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "author_id",
+        "genre_id",
+        "title",
+        "isbn",
+        "pages",
+        "stock",
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function genre(): BelongsTo
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function loans(): HasMany
+    {
+        return $this->hasMany(Loan::class);
+    }
 }
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\Userscope;
+use App\Traits\HasUserId;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Loan extends Model
 {
     use HasFactory;
+    use HasUserId;
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::addGlobalScope(new Userscope());
+    }
+
+    protected $fillable = [
+        "user_id",
+        "book_id",
+        "loaned_at",
+        "returned_at",
+        "overdue_at",
+        "is_returned",
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            "loaned_at" => "datetime",
+            "returned_at" => "datetime",
+            "overdue_at" => "datetime",
+            "is_returned" => "boolean",
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function isOwner(): bool
+    {
+        return $this->user_id === auth()->id();
+    }
 }
