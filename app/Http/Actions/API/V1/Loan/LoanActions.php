e3685a8ae241055344014d08f5a90d092ae21221
EntrenaDev <manuel.entrena@gmail.com>
2024-09-09 19:36:14 +0200

    refactor: :bricks: Added actions to loan controller

---
+++ app/Http/Actions/API/V1/Loan/LoanActions.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Actions\API\V1\Loan;
+use App\Exceptions\API\V1\UpdateLoanException;
+use App\Http\Requests\API\V1\Loan\StoreLoanRequest;
+use App\Exceptions\API\V1\CreateLoanException;
+use App\Models\Loan;
+use DB;
+
+class LoanActions
+{
+    public function createLoanAndDecreaseStock(StoreLoanRequest $request): Loan
+    {
+        return DB::transaction(function () use ($request) {
+            $loan = Loan::create([
+                'book_id' => $request->book_id,
+                'loaned_at' => now(),
+                'overdue_at' => now()->addDays(config('library.days_of_loans')),
+                'returned_at' => null,
+                'is_returned' => false,
+            ]);
+
+            if (!$loan) {
+                throw new CreateLoanException();
+            }
+
+            $isStockUpdate = $this->decreaseStock($loan);
+
+            if (!$isStockUpdate) {
+                throw new CreateLoanException();
+            }
+
+            return $loan;
+        });
+    }
+
+    public function decreaseStock(Loan $loan): bool
+    {
+        return $loan->book->update([
+            'stock' => $loan->book->stock - config('library.less_stock'),
+        ]);
+    }
+
+    public function updateLoanAndAddStock(Loan $loan)
+    {
+        return DB::transaction(function () use ($loan) {
+            $isLoanUpdate = $loan->update([
+                'returned_at' => now(),
+                'is_returned' => true,
+            ]);
+
+            if (!$isLoanUpdate) {
+                throw new UpdateLoanException();
+            }
+
+            $isStockUpdate = $this->increaseStock($loan);
+
+            if (!$isStockUpdate) {
+                throw new UpdateLoanException();
+            }
+
+            return $loan;
+        });
+    }
+
+    public function increaseStock($loan): bool
+    {
+        return $loan->book->update([
+            'stock' => $loan->book->stock + config('library.more_stock'),
+        ]);
+    }
+}
--- app/Http/Controllers/API/V1/Library/LoanController.php
+++ app/Http/Controllers/API/V1/Library/LoanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API\V1\Library;
 
+use App\Http\Actions\API\V1\Loan\LoanActions;
 use App\Http\Controllers\API\V1\Controller;
 use App\Http\Requests\API\V1\Loan\StoreLoanRequest;
 use App\Http\Resources\API\V1\Loan\LoanCollection;
@@ -14,9 +15,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LoanController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): JsonResponse
     {
         return ApiResponseService::success(
@@ -25,10 +23,7 @@ class LoanController extends Controller
         );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreLoanRequest $request): JsonResponse
+    public function store(StoreLoanRequest $request, LoanActions $actions): JsonResponse
     {
         if (!Book::find($request->book_id)->stock) {
             return ApiResponseService::error(
@@ -37,17 +32,7 @@ class LoanController extends Controller
             );
         }
 
-        $loan = Loan::create([
-            'book_id' => $request->book_id,
-            'loaned_at' => now(),
-            'overdue_at' => now()->addDays(config('library.days_of_loans')),
-            'returned_at' => null,
-            'is_returned' => false,
-        ]);
-
-        $loan->book->update([
-            'stock' => $loan->book->stock - config('library.less_stock'),
-        ]);
+        $loan = $actions->createLoanAndDecreaseStock($request);
 
         return ApiResponseService::success(
             new LoanResource($loan->load('book', 'book.author', 'book.genre')),
@@ -56,9 +41,6 @@ class LoanController extends Controller
         );
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Loan $loan): JsonResponse
     {
         return ApiResponseService::success(
@@ -68,9 +50,8 @@ class LoanController extends Controller
     }
 
 
-    public function returnLoan(Loan $loan): JsonResponse
+    public function returnLoan(Loan $loan, LoanActions $actions): JsonResponse
     {
-
         if ($loan->is_returned) {
             return ApiResponseService::error(
                 'The book was already returned',
@@ -85,14 +66,7 @@ class LoanController extends Controller
             );
         }
 
-        $loan->update([
-            'is_returned' => true,
-            'returned_at' => now(),
-        ]);
-
-        $loan->book->update([
-            'stock' => $loan->book->stock + config('library.more_stock'),
-        ]);
+        $loan = $actions->updateLoanAndAddStock($loan);
 
         return ApiResponseService::success(
             new LoanResource($loan->load('book', 'book.author', 'book.genre')),
