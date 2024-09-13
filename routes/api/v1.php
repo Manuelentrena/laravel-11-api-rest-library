8b2a9f590a7e68c5befae1585d6d2395fa5ff542
EntrenaDev <manuel.entrena@gmail.com>
2024-09-13 17:10:02 +0200

    docs: :truck: Named prefix routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -12,10 +12,10 @@ use App\Http\Controllers\API\V1\Auth\{
 use App\Http\Controllers\API\V1\Library\AuthorController;
 
 Route::prefix('auth')->group(function () {
-    Route::post('register', RegisterController::class);
-    Route::post('login', LoginController::class);
+    Route::post('register', RegisterController::class)->name('auth.register');
+    Route::post('login', LoginController::class)->name('auth.login');
     Route::group(['middleware' => 'auth:sanctum'], function () {
-        Route::post('logout', LogoutController::class);
+        Route::post('logout', LogoutController::class)->name('auth.logout');
     });
 });
 
@@ -23,8 +23,8 @@ Route::prefix('library')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('authors', AuthorController::class);
     Route::apiResource('genres', GenreController::class);
     Route::apiResource('books', BookController::class);
-    Route::patch('books/{book}/stock', [BookController::class, 'updateStock']);
+    Route::patch('books/{book}/stock', [BookController::class, 'updateStock'])->name('books.update_stock');
     Route::apiResource('loans', LoanController::class)
         ->only(['index', 'store', 'show']);
-    Route::patch('loans/{loan}/return', [LoanController::class, 'returnLoan']);
+    Route::patch('loans/{loan}/return', [LoanController::class, 'returnLoan'])->name('loans.return_loan');
 });
